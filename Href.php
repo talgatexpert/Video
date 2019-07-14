453544728d138896bb4e5c27d8f8e8f5520f29d4
talgatexpert <49163220+talgatexpert@users.noreply.github.com>
2019-07-14 22:35:27 +0300

    My site

---
+++ Href.php
@@ -0,0 +1,101 @@
+User
+  <!-- Font Awesome -->
+css/fontawesome.css
+css/font-awesome.min.css
+
+    <!-- Google Fonts -->
+assets/fonts
+
+assets/css/bootstrap.css
+assets/css/index.css
+
+
+
+
+
+
+
+
+
+    admin
+
+   
+   fonts
+   css/bootstrap.min.css
+   css/fontawesome.css
+   css/font-awesome.min.css
+   css/owl.carousel.css
+   css/owl.theme.css
+   css/owl.transitions.css
+   css/meanmenu/meanmenu.min.css
+   css/animate.css
+   css/summernote/summernote.css
+   css/themesaller-forms.css
+   css/normalize.css
+   css/scrollbar/jquery.mCustomScrollbar.min.css
+   css/notika-custom-icon.css
+   css/bootstrap-select/bootstrap-select.css
+   css/datapicker/datepicker3.css
+   css/color-picker/farbtastic.css
+   css/chosen/chosen.css
+   css/notification/notification.css
+   css/dropzone/dropzone.css
+   css/wave/waves.min.css
+   css/wave/button.css
+   css/main.css
+   css/style.css
+   css/responsive.css
+   css/jquery.dataTables.min.css
+   css/scrollbar/jquery.mCustomScrollbar.min.css
+
+  js/vendor/modernizr-2.8.3.min.js
+  js/vendor/jquery-1.12.4.min.js
+  js/bootstrap.min.js
+  js/wow.min.js
+  js/jquery-price-slider.js
+  js/owl.carousel.min.js
+  js/jquery.scrollUp.min.js
+  js/meanmenu/jquery.meanmenu.js
+  js/counterup/jquery.counterup.min.js
+  js/counterup/waypoints.min.js
+  js/counterup/counterup-active.js
+  js/scrollbar/jquery.mCustomScrollbar.concat.min.js
+  js/sparkline/jquery.sparkline.min.js
+  js/sparkline/sparkline-active.js
+  js/flot/jquery.flot.js
+  js/flot/jquery.flot.resize.js
+  js/flot/flot-active.js
+  js/knob/jquery.knob.js
+  js/knob/jquery.appear.js
+  js/knob/knob-active.js
+  js/jasny-bootstrap.min.js
+  js/chat/jquery.chat.js
+  js/todo/jquery.todo.js
+  js/icheck/icheck.min.js
+  js/icheck/icheck-active.js
+  js/rangle-slider/jquery-ui-1.10.4.custom.min.js
+  js/rangle-slider/jquery-ui-touch-punch.min.js
+  js/rangle-slider/rangle-active.js
+  js/datapicker/bootstrap-datepicker.js
+  js/datapicker/datepicker-active.js
+  js/bootstrap-select/bootstrap-select.js
+  js/color-picker/farbtastic.min.js
+  js/color-picker/color-picker.js
+  js/notification/bootstrap-growl.min.js
+  js/notification/notification-active.js
+  js/summernote/summernote-updated.min.js
+  js/summernote/summernote-active.js
+  js/dropzone/dropzone.js
+  js/wave/waves.min.js
+  js/wave/wave-active.js
+  js/chosen/chosen.jquery.js
+  js/chat/jquery.chat.js
+  js/todo/jquery.todo.js
+  js/plugins.js
+  js/data-table/jquery.dataTables.min.js
+  js/data-table/data-table-act.js
+  js/main.js
+  js/tawk-chat.js
+
+
+
